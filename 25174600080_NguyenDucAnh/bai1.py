9e9fc2b48c5236e250cd11c3bdb1b61f9d4a82ff
ndanh25174600080-ops <ndanh.25174600080@sv.uneti.edu.vn>
2026-04-28 21:18:19 +0700

    em nop bai lab 8 ạ

---
+++ 25174600080_NguyenDucAnh/bai1.py
@@ -0,0 +1,6 @@
+def luy_thua():
+    x = int(input("Nhap so x:"))
+    n = int(input("Nhap so mu n:"))
+    print('Luy thua cua ',x,'mũ',n,'=',x**n)
+    return
+luy_thua()
---
+++ 25174600080_NguyenDucAnh/bai1p2.py
@@ -0,0 +1,8 @@
+def value(x):
+    return x + 1
+
+# Nhập số từ bàn phím
+n = int(input("Nhập số nguyên: "))
+
+# In kết quả
+print("Số kế tiếp là:", value(n))
