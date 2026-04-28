6dd1c049219a29d7160905ccf59f21bf97bf5913
ndanh25174600080-ops <ndanh.25174600080@sv.uneti.edu.vn>
2026-04-28 21:21:58 +0700

    Delete 25174600080_NguyenDucAnh directory

--- 25174600080_NguyenDucAnh/bai1.py
+++
@@ -1,6 +0,0 @@
-def luy_thua():
-    x = int(input("Nhap so x:"))
-    n = int(input("Nhap so mu n:"))
-    print('Luy thua cua ',x,'mũ',n,'=',x**n)
-    return
-luy_thua()
--- 25174600080_NguyenDucAnh/bai1p2.py
+++
@@ -1,8 +0,0 @@
-def value(x):
-    return x + 1
-
-# Nhập số từ bàn phím
-n = int(input("Nhập số nguyên: "))
-
-# In kết quả
-print("Số kế tiếp là:", value(n))
